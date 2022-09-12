3be65ed214e7353e923b9a251db6dd25cce543ef
Alexander von Below <alex@vonbelow.com>
2022-09-12 15:31:47 +0200

    Initial Commit

---
+++ MatterCommissionerPOC/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MatterCommissionerPOC
+//
+//  Created by Alexander von Below on 12.09.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MatterCommissionerPOC/MatterCommissionerPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatterCommissionerPOCApp.swift
+//  MatterCommissionerPOC
+//
+//  Created by Alexander von Below on 12.09.22.
+//
+
+import SwiftUI
+
+@main
+struct MatterCommissionerPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
